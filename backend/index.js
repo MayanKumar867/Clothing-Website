6bfdd8a379db324e2e8de14a2912c83a1f3a62f4
Mayank Kumar <115584139+MayanKumar867@users.noreply.github.com>
2025-09-08 22:52:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin:["https://vsr-fashion-frontend.onrender.com", "http://localhost:5174" , "http://localhost:5175"],
+  origin:["https://vsr-fashion-frontend.onrender.com", "http://localhost:5174" , "https://clothing-website-admin-nszy.onrender.com"],
   credentials:true
 }))
 
