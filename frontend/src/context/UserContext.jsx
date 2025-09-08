5753eacdee95f187f36da389455b8758b455268f
Mayank Kumar <115584139+MayanKumar867@users.noreply.github.com>
2025-09-08 18:26:42 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { authDataContext } from "./authContext";
+import { authDataContext } from "./AuthContext";
 import axios from "axios";
 
 
